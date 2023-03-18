080cbcb48b31706e72c738f5dbd1a97dcde96f78
Oskar Arce <oskar.arce@gmail.com>
2023-03-18 01:41:14 +0100

    feat: add nav indicator

--- src/components/Heaeder.tsx
+++ src/components/Heaeder.tsx
@@ -1,6 +1,9 @@
-import { useLocation, A } from "solid-start";
+import { useIsRouting, useLocation, A } from "solid-start";
+import { Show } from "solid-js";
+import NProgress from "./NProgress";
 
 export default function Header() {
+  const isRouting = useIsRouting();
   const location = useLocation();
   const active = (path: string) =>
     path == location.pathname
@@ -8,12 +11,15 @@ export default function Header() {
       : "border-transparent hover:border-sky-600";
 
   return (
-    <nav class="bg-sky-800">
+    <nav class="bg-sky-800 relative">
       <ul class="container flex items-center p-3 text-gray-200">
         <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
           <A href="/">Podcaster</A>
         </li>
       </ul>
+      <Show when={isRouting()}>
+        <NProgress />
+      </Show>
     </nav>
   );
 }
